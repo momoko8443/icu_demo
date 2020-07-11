90411f81a8183df39744409ae3072eb0d92c912e
Jake.Zheng <Huibin.zheng@hpe.com>
2020-07-11 10:25:31 +0800

    remove sh

--- pack_dep_files.sh
+++
@@ -1,9 +0,0 @@
-# 在源代码根目录，使用git status命令获取已修改文件的列表
-git status | grep modified | awk '{print $2}' > list.txt
-# 在当前目录下，创建temp目录
-# 将已修改文件列表逐一复制到当前目录下的temp目录
-xargs -0opt ./list.txt cp --parents -t ./temp
-
-# 将temp目录下的所有文件打包为modules.tar.gz
-cd temp
-tar -czf modules.tar.gz *
